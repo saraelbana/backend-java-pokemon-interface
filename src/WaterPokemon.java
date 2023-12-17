0c4ca80dfcf1dda83823bd9e48bf323adb765416
saraelbana <matika20@gmail.com>
2023-12-17 23:44:44 +0200

    start implementing WaterPokemon class

---
+++ src/WaterPokemon.java
@@ -0,0 +1,5 @@
+public class WaterPokemon extends Pokemon{
+    WaterPokemon(String name, float weight, float height, String food, String sound){
+        super( name,  weight,  height,  food,  sound);
+    }
+}
