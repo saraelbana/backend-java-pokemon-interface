14270585cd5ae57a4060df40e773a11bb2100039
saraelbana <matika20@gmail.com>
2023-12-18 00:25:03 +0200

    adding overiden methods in GrassPokemon class

--- src/WaterPokemon.java
+++ src/WaterPokemon.java
@@ -2,4 +2,79 @@ public class WaterPokemon extends Pokemon{
     WaterPokemon(String name, float weight, float height, String food, String sound){
         super( name,  weight,  height,  food,  sound);
     }
+
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+    }
+
+    @Override
+    public void setLevel(int level) {
+        super.setLevel(level);
+    }
+
+    @Override
+    public void setHeight(float height) {
+        super.setHeight(height);
+    }
+
+    @Override
+    public void setWeight(float weight) {
+        super.setWeight(weight);
+    }
+
+    @Override
+    public void setFood(String food) {
+        super.setFood(food);
+    }
+
+    @Override
+    public void setSound(String sound) {
+        super.setSound(sound);
+    }
+
+    @Override
+    public void setDefence(String defence) {
+        super.setDefence(defence);
+    }
+
+    @Override
+    public void setAttack(String attack) {
+        super.setAttack(attack);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public int getLevel() {
+        return super.getLevel();
+    }
+
+    @Override
+    public float getHeight() {
+        return super.getHeight();
+    }
+
+    @Override
+    public float getWeight() {
+        return super.getWeight();
+    }
+
+    @Override
+    public String getFood() {
+        return super.getFood();
+    }
+
+    @Override
+    public String getSound() {
+        return super.getSound();
+    }
+
+    @Override
+    public String getDefence() {
+        return super.getDefence();
+    }
 }
