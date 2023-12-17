e87848905c4b79cc97bc1f493729254710b37874
saraelbana <matika20@gmail.com>
2023-12-17 23:48:05 +0200

    start implementing Grass/ElectricPokemon class

---
+++ src/GrassPokemon.java
@@ -0,0 +1,5 @@
+public class GrassPokemon extends Pokemon {
+    GrassPokemon(String name, float weight, float height, String food, String sound){
+        super( name,  weight,  height,  food,  sound);
+    }
+}
